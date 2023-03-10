6b1213cc21b6e27c9f5850f4965d9569b915a2f8
Duccio Rocchini <ducciorocchini@gmail.com>
2023-03-10 17:56:37 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -4,3 +4,25 @@
 install.packages("raster")
 
 library(raster)
+
+# Import data, setting the working directory
+setwd("~/lab/") # Linux
+# setwd("/Users/emma/desktop/lab") #mac
+# setwd("C:/lab/") # windows
+
+l2011 <- brick("p224r63_2011_masked.grd")
+
+# plotting the data in a savage manner
+plot(l2011)
+
+
+cl <- colorRampPalette(c("red","orange","yellow"))(100) # 100 sono le sfumature
+plot(l2011, col=cl)
+
+# plotting one element
+plot(l2011[[4]], col=cl)
+
+plot(l2011$B4_sre, col=cl)
+
+nir <- l2011[[4]] # or: nir <- l2011$B4_sre
+plot(nir, col=cl)
