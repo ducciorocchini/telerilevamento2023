b364a192bda93d426613e5c668320f75b854b2e3
Duccio Rocchini <ducciorocchini@gmail.com>
2023-03-16 17:49:50 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -26,3 +26,50 @@ plot(l2011$B4_sre, col=cl)
 
 nir <- l2011[[4]] # or: nir <- l2011$B4_sre
 plot(nir, col=cl)
+
+# Exercise: change the colour gamut for all the images
+cl <- colorRampPalette(c("cyan","azure","darkorchid","aquamarine"))(100) # 100 sono le sfumature
+plot(l2011, col=cl)
+
+# Exercise: plot the NIR band
+# b1=blue
+# b2=green
+# b3=red
+# b4=NIR
+
+plot(l2011[[4]], col=cl)
+plot(l2011$B4_sre, col=cl)
+
+#dev.off() # it closes graphs
+
+# Export graphs in R
+pdf("myfirstgraph.pdf")
+plot(l2011$B4_sre, col=cl)
+dev.off()
+
+# Plotting several bands in a multiframe
+par(mfrow=c(2,1))
+plot(l2011[[3]], col=cl)
+plot(l2011[[4]], col=cl)
+
+# Plotting the first 4 layers / bands
+par(mfrow=c(2,2))
+
+# blue
+clb <- colorRampPalette(c("blue4","blue2","light blue"))(100) # 100 sono le sfumature
+plot(l2011[[1]], col=clb)
+
+clg <- colorRampPalette(c("chartreuse4","chartreuse2","chartreuse"))(100) # 100 sono le sfumature
+plot(l2011[[2]], col=clg)
+
+clr <- colorRampPalette(c("coral3","coral1","coral"))(100) # 100 sono le sfumature
+plot(l2011[[3]], col=clr)
+
+cln <- colorRampPalette(c("darkorchid4","darkorchid2","darkorchid4"))(100) # 100 sono le sfumature
+plot(l2011[[4]], col=cln)
+
+
+
+
+
+
