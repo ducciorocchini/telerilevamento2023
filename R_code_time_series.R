10ff2458b343f31a0c4962265c25fe53214c196e
Duccio Rocchini <ducciorocchini@gmail.com>
2023-03-31 17:35:57 +0200

    Update R_code_time_series.R

--- R_code_time_series.R
+++ R_code_time_series.R
@@ -6,7 +6,7 @@ setwd("~/lab/greenland") # Linux
 # setwd("C:/lab/greenland") # Windows
 # setwd("/Users/name/Desktop/lab/greenland") # Mac
 
-lst_2000 <- raster("lst_2000.tif")
+lst_2000 <- raster("lst_2000.tif") # rast for those using the terra package
 plot(lst_2000)
 
 # Exercise: import all the data
@@ -21,3 +21,15 @@ rlist
 
 import <- lapply(rlist, raster)
 import
+
+TGr <- stack(import) # in terra::c
+TGr
+
+plotRGB(TGr, r=1, g=2, b=3, stretch="lin") 
+plotRGB(TGr, r=2, g=3, b=4, stretch="lin") 
+
+# difference:
+dift = TGr[[2]] - TGr[[1]]
+
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+plot(TGr, col=cl)
