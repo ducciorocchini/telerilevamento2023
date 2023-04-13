718774de34fd458698fbb772e680ee34836b1336
Duccio Rocchini <ducciorocchini@gmail.com>
2023-04-13 17:06:53 +0200

    Update R_code_time_series.R

--- R_code_time_series.R
+++ R_code_time_series.R
@@ -33,3 +33,49 @@ dift = TGr[[2]] - TGr[[1]]
 
 cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
 plot(TGr, col=cl)
+
+#----- Exercise 2: Eurpean NO2
+
+setwd("~/lab/en") # Linux
+# setwd("C:/lab/en") # Windows
+# setwd("/Users/name/Desktop/lab/en") # Mac
+
+# importing a file
+en_first <- raster("EN_0001.png")
+
+cl <- colorRampPalette(c('red','orange','yellow'))(100) #
+plot(en_first, col=cl)
+
+rlist <- list.files(pattern="EN")
+rlist
+
+import <- lapply(rlist, raster)
+import
+
+EN <- stack(import) # in terra::c
+EN
+
+plot(EN, col=cl)
+
+# check
+par(mfrow=c(1,2))
+plot(en_first, col=cl)
+plot(EN[[1]], col=cl)
+
+# check 2
+difcheck = en_first - EN[[1]]
+difcheck
+plot(difcheck)
+
+# first and last data
+par(mfrow=c(1,2))
+plot(EN[[1]], col=cl)
+plot(EN[[13]], col=cl)
+
+difen = EN[[1]]-EN[[13]]
+cldif <- colorRampPalette(c('blue','white','red'))(100) #
+plot(difen, col=cldif)
+
+# plotRGB
+plotRGB(EN, 1, 7, 13, stretch="lin")
+plotRGB(EN, 1, 7, 13, stretch="hist")
