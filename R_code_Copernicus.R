650f0cf8a732f1849e1a371654169ea083a9e424
Duccio Rocchini <ducciorocchini@gmail.com>
2023-05-11 17:48:54 +0200

    Create R_code_Copernicus.R

---
+++ R_code_Copernicus.R
@@ -0,0 +1,11 @@
+# R ocde for downloading and visualizing Copernicus data
+
+# install.packages("ncdf4")
+library(ncdf4)
+library(raster)
+
+setwd("~/lab/") # Linux
+# setwd("C:/lab/") # Windows
+# setwd("/Users/name/Desktop/lab/") # Mac
+
+# dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
