52eb46903cc6e7160421de9810de0e2d586ef7d6
Duccio Rocchini <ducciorocchini@gmail.com>
2023-05-11 18:00:43 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -9,3 +9,6 @@ setwd("~/lab/") # Linux
 # setwd("/Users/name/Desktop/lab/") # Mac
 
 # dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
+
+sc <- raster("c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
+plot(sc)
