68eaafa4e0b74dbecacd3f589f3838b17ac478b4
Duccio Rocchini <ducciorocchini@gmail.com>
2023-05-12 17:32:27 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -3,6 +3,8 @@
 # install.packages("ncdf4")
 library(ncdf4)
 library(raster)
+library(ggplot2)
+library(viridis)
 
 setwd("~/lab/") # Linux
 # setwd("C:/lab/") # Windows
@@ -16,5 +18,24 @@ plot(sc)
 # with the raster package -> RasterLayer
 # with the terra package -> SpatRaster: it maintains the uncertainty data, so: 2 layers
 
+scd <- as.data.frame(sc, xy=T)
+
+ggplot() +
+geom_raster(scd, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
+ggtitle("Soil Moisture from Copernicus")
+
+# Cropping an image
+ext <- c(23, 30, 62, 68)
+sc.crop <- crop(sc, ext)
+
+# Exercise: plot via ggplot the cropped image
+sc.crop.d <- as.data.frame(sc.crop, xy=T)
+head(sc.crop.d)
+names(sc.crop.d)
+
+ggplot() +
+geom_raster(sc.crop.d, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
+ggtitle("Cropped Soil Moisture from Copernicus") +
+scale_fill_viridis(option="cividis")
 
 
